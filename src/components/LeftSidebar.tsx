7c42934c229a48ecd289ca82572b1cd88554debf
Arya Priema <99aryaprima@gmail.com>
2023-09-28 11:57:47 +0700

    update: LeftSidebar - styles

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -21,73 +21,75 @@ const LeftSidebar = ({ username, name, imageUrl }: LeftSidebarProps) => {
     const openTweetModal = useTweetModal(state => state.onOpen)
 
     return (
-        <aside className="w-fit h-screen p-3 border-r border-r-gray-300 max-sm:hidden sm:flex flex-col justify-between">
-            <ul className="flex flex-col space-y-6">
-                {links.map(link => {
-                    if (!link.href) link.href = `/${username}`
+        <aside className="w-fit h-screen p-3 border-r border-r-gray-300 max-sm:hidden sm:flex">
+            <section className="overflow-y-auto space-y-20 flex flex-col justify-between">
+                <ul className="flex flex-col space-y-6">
+                    {links.map(link => {
+                        if (!link.href) link.href = `/${username}`
 
-                    const isNotLogo = link.title !== "X Logo";
-                    const isSamePath = isNotLogo && link.href === pathname;
+                        const isNotLogo = link.title !== "X Logo";
+                        const isSamePath = isNotLogo && link.href === pathname;
 
-                    return (
-                        <li
-                            key={link.title}
-                            className={cn("w-fit rounded-full overflow-hidden", isSamePath && "bg-black-200 font-bold")}
-                        >
-                            <Link href={link.href} className="flex flex-row items-center gap-x-6 tracking-wider text-xl max-lg:p-3 lg:py-3 lg:px-5 hover:bg-black-200 transition">
-                                <Image src={link.icon} alt={link.title} width={30} height={30} className="object-contain w-[30px] h-[30px]" />
-                                {isNotLogo && (
-                                    <span className="max-lg:hidden lg:inline">
-                                        {link.title}
-                                    </span>
-                                )}
-                            </Link>
-                        </li>
-                    )
-                })}
-                <Button variant="primary" className="max-lg:w-fit lg:w-full p-3" onClick={openTweetModal}>
-                    <span className="max-lg:hidden lg:inline">
-                        Tweet
-                    </span>
-                    <span className="max-lg:inline lg:hidden">
-                        <Plus size={30} />
-                    </span>
-                </Button>
-            </ul>
-            <DropdownMenu>
-                <DropdownMenuTrigger
-                    className="max-lg:p-3 lg:py-2 lg:px-5 rounded-full hover:bg-black-200 transition flex items-center gap-x-20 cursor-pointer !outline-none !w-full"
-                >
-                    <div className="max-lg:hidden lg:flex items-center gap-x-2">
-                        <Image
-                            src={imageUrl}
-                            alt={name}
-                            width={50}
-                            height={50}
-                            priority
-                            className="object-cover rounded-full w-[50px] h-[50px]"
-                        />
-                        <div className="flex flex-col items-start">
-                            <h5 className="font-bold text-white tracking-wide whitespace-nowrap">{name.length > 10 ? `${name.slice(0, 10)} ...` : name}</h5>
-                            <span className="text-gray-200 font-bold whitespace-nowrap">@{username.length > 8 ? `${username.slice(0, 8)} ...` : username}</span>
+                        return (
+                            <li
+                                key={link.title}
+                                className={cn("w-fit rounded-full overflow-hidden", isSamePath && "bg-black-200 font-bold")}
+                            >
+                                <Link href={link.href} className="flex flex-row items-center gap-x-6 tracking-wider text-xl max-xl:p-3 xl:py-3 xl:px-5 hover:bg-black-200 transition">
+                                    <Image src={link.icon} alt={link.title} width={30} height={30} className="object-contain w-[30px] h-[30px]" />
+                                    {isNotLogo && (
+                                        <span className="max-xl:hidden xl:inline">
+                                            {link.title}
+                                        </span>
+                                    )}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                    <Button variant="primary" className="max-xl:w-fit xl:w-full p-3" onClick={openTweetModal}>
+                        <span className="max-xl:hidden xl:inline">
+                            Tweet
+                        </span>
+                        <span className="max-xl:inline xl:hidden">
+                            <Plus size={30} />
+                        </span>
+                    </Button>
+                </ul>
+                <DropdownMenu>
+                    <DropdownMenuTrigger
+                        className="max-xl:p-3 xl:py-2 xl:px-5 rounded-full hover:bg-black-200 transition flex items-center gap-x-20 cursor-pointer !outline-none !w-full"
+                    >
+                        <div className="max-xl:hidden xl:flex items-center gap-x-2">
+                            <Image
+                                src={imageUrl}
+                                alt={name}
+                                width={50}
+                                height={50}
+                                priority
+                                className="object-cover rounded-full w-[50px] h-[50px]"
+                            />
+                            <div className="flex flex-col items-start">
+                                <h5 className="font-bold text-white tracking-wide whitespace-nowrap">{name.length > 10 ? `${name.slice(0, 10)} ...` : name}</h5>
+                                <span className="text-gray-200 font-bold whitespace-nowrap">@{username.length > 8 ? `${username.slice(0, 8)} ...` : username}</span>
+                            </div>
                         </div>
-                    </div>
-                    <MoreHorizontal size={30} className="text-white" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuItem>
-                        <SignOutButton
-                            signOutCallback={() => {
-                                window.location.href = "/"
-                            }}
-                        >
-                            <p>
-                                Log out @{username}
-                            </p>
-                        </SignOutButton>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+                        <MoreHorizontal size={30} className="text-white" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>
+                            <SignOutButton
+                                signOutCallback={() => {
+                                    window.location.href = "/"
+                                }}
+                            >
+                                <p>
+                                    Log out @{username}
+                                </p>
+                            </SignOutButton>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </section>
         </aside>
     )
 }
