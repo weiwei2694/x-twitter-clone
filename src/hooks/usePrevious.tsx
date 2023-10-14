1bf3cd08f7aa5099dde3fee462733a3bf019f21e
Arya Priema <99aryaprima@gmail.com>
2023-10-14 16:06:30 +0700

    feat: hooks - create usePrevious

---
+++ src/hooks/usePrevious.tsx
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+interface State {
+  navigationHistory: string[];
+}
+
+interface Action {
+  addToNavigationHistory: (url: string) => void;
+  goBack: () => void;
+  clearNavigationHistory: () => void;
+}
+
+export const usePrevious = create<State & Action>(set => ({
+  navigationHistory: [],
+  addToNavigationHistory: (url) => set((state) => {
+    const len = state.navigationHistory.length - 1;
+    const leadingValue = state.navigationHistory[len];
+
+    if (leadingValue === url) return { navigationHistory: [...state.navigationHistory]};
+
+    return { navigationHistory: [...state.navigationHistory, url] }
+  }),
+  goBack: () => set((state) => ({ navigationHistory: state.navigationHistory.slice(0, -1) })),
+  clearNavigationHistory: () => set({ navigationHistory: [] }),
+}))
