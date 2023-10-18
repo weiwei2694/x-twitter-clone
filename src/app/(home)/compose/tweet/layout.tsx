1d4a39d42d954b6d49d921612c1861c1b2427851
Arya Priema <99aryaprima@gmail.com>
2023-10-18 12:09:34 +0700

    fix: compose/tweet - add layout.tsx to enchance loading ui

---
+++ src/app/(home)/compose/tweet/layout.tsx
@@ -0,0 +1,18 @@
+import Loading from '@/components/sharing/Loading'
+import { ReactNode, Suspense } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+const layout = ({ children }: Props) => {
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        {children}
+      </Suspense>
+    </>
+  )
+}
+
+export default layout
