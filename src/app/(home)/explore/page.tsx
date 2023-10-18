1972f274cbb1a90fb8372dbb757045da2e3f0ba1
Arya Priema <99aryaprima@gmail.com>
2023-10-18 11:51:45 +0700

    feat: /explore/page.tsx - added pagination

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,28 +1,59 @@
 import { getUserAction, getUsersAction } from '@/actions/user.action'
 import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
-import ShowUsersData from '@/components/explore/ShowUsersData'
+import { isValidPage } from '@/lib/utils'
+import Users from '@/components/cards/Users'
+import PaginationButtons from '@/components/sharing/PaginationButtons'
 
-const Page = async () => {
+interface Props {
+  searchParams: {
+    page: string;
+  }
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const { page: qPage } = searchParams;
+  const page = isValidPage(qPage);
+
   const clerkUser = await currentUser()
   if (!clerkUser) return null;
 
   const user = await getUserAction(clerkUser.id)
   if (!user) redirect('/');
 
-  let users = await getUsersAction({
+  const users = await getUsersAction({
     userId: user.id,
-    size: 20,
+    size: 30,
+    page
   });
-  if (!users?.length) users = [];
 
   return (
-    <>
-      <ShowUsersData
-        initialDataUsers={users!}
-        user={user}
-      />
-    </>
+    users?.data.length ? (
+      <>
+        {users.data.map(dataUser => (
+          <Users
+            key={dataUser.id}
+            username={dataUser.username}
+            name={dataUser.name}
+            imageUrl={dataUser.imageUrl}
+            userId={dataUser.id}
+            currentUser={user}
+          />
+        ))}
+
+        <PaginationButtons
+          currentPage={page}
+          currentPath={"/explore"}
+          hasNext={users.hasNext}
+        />
+      </>
+    ) : (
+      <div className="flex justify-center py-4 px-3">
+        <div className="flex flex-col items-start">
+          <p className="font-normal text-gray-200">Try searching for something else</p>
+        </div>
+      </div>
+    )
   )
 }
 
