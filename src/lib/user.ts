d5ad99ebebf580cdbb25ebf7979ed05bd44d78b4
Arya Priema <99aryaprima@gmail.com>
2023-10-19 13:38:48 +0700

    feat: lib/user.ts - add JSDoc

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -6,6 +6,12 @@ import {
 import { toastOptions } from "./utils";
 import { followUserNotificationAction } from "@/actions/notification.action";
 
+/**
+ * Toggles the follow status of a user.
+ *
+ * @param {toggleFollowUserProps} toggleFollowUserProps - The properties needed for toggling the follow status.
+ * @return {void} This function does not return anything.
+ */
 export const toggleFollowUser = ({
 	isPending,
 	startTransition,
@@ -15,7 +21,7 @@ export const toggleFollowUser = ({
 	followed,
 	userId,
 	currentUserId,
-}: toggleFollowUserProps) => {
+}: toggleFollowUserProps): void => {
 	if (isPending || userId === currentUserId) return;
 
 	startTransition(() => {
@@ -26,15 +32,22 @@ export const toggleFollowUser = ({
 			parentIdUser: currentUserId,
 			path,
 		});
-	})
+	});
 
 	const message = followed
 		? `You unfollowed ${username}`
-		: `You followed ${username}`
+		: `You followed ${username}`;
 
 	toast(message, toastOptions);
 };
 
+/**
+ * Copies the user's link to the clipboard and displays a toast message.
+ *
+ * @param {CopyLinkUserProps} props - The properties needed to copy the link.
+ * @param {ToastFunction} props.toast - The function to display toast messages.
+ * @param {string} props.username - The username to be included in the link.
+ */
 export const copyLinkUser = ({ toast, username }: CopyLinkUserProps) => {
 	const url = process.env.NEXT_PUBLIC_NEXT_URL;
 	navigator.clipboard.writeText(`${url}/${username}`);
