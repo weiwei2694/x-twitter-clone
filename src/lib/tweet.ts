e08378699382e11664c6f831d0b2601dc0c82dac
Arya Priema <99aryaprima@gmail.com>
2023-10-19 13:41:56 +0700

    feat: lib/tweet.ts - add JSDoc

--- src/lib/tweet.ts
+++ src/lib/tweet.ts
@@ -12,6 +12,16 @@ import {
 import { toastOptions } from "./utils";
 import { likePostNotificationAction } from "@/actions/notification.action";
 
+/**
+ * Deletes a tweet.
+ *
+ * @param {DeleteTweetProps} props - The properties of the tweet deletion.
+ * @param {boolean} props.isPending - Indicates if the tweet deletion is pending.
+ * @param {function} props.startTransition - The function to start a transition.
+ * @param {function} props.toast - The function to display a toast message.
+ * @param {string} props.path - The path of the tweet.
+ * @param {string} props.id - The ID of the tweet.
+ */
 export const deleteTweet = ({
 	isPending,
 	startTransition,
@@ -28,6 +38,19 @@ export const deleteTweet = ({
 	});
 };
 
+/**
+ * Toggles the bookmark state of a tweet.
+ *
+ * @param {ToggleBookmarkTweetProps} props - The properties object containing the necessary information for bookmarking a tweet.
+ * @param {boolean} props.isPending - A flag indicating whether the bookmark action is pending.
+ * @param {function} props.startTransition - A function to initiate a transition.
+ * @param {function} props.toast - A function to display a toast message.
+ * @param {string} props.path - The path of the tweet.
+ * @param {boolean} props.bookmark - A flag indicating whether the tweet is bookmarked.
+ * @param {string} props.userId - The ID of the user.
+ * @param {string} props.threadId - The ID of the tweet thread.
+ * @return {void}
+ */
 export const toggleBookmarkTweet = ({
 	isPending,
 	startTransition,
@@ -36,7 +59,7 @@ export const toggleBookmarkTweet = ({
 	bookmark,
 	userId,
 	threadId,
-}: ToggleBookmarkTweetProps) => {
+}: ToggleBookmarkTweetProps): void => {
 	if (isPending) return;
 
 	const message = bookmark
@@ -54,6 +77,14 @@ export const toggleBookmarkTweet = ({
 	});
 };
 
+/**
+ * Copies the link to a tweet and displays a toast message.
+ *
+ * @param {CopyLinkTweetProps} props - The properties for copying the link to a tweet.
+ * @param {string} props.toast - The toast message to display.
+ * @param {string} props.username - The username of the tweet.
+ * @param {string} props.tweetId - The ID of the tweet.
+ */
 export const copyLinkTweet = ({
 	toast,
 	username,
@@ -65,6 +96,19 @@ export const copyLinkTweet = ({
 	toast("Copied to clipboard", toastOptions);
 };
 
+/**
+ * Toggles the like status of a tweet and sends a like notification if necessary.
+ *
+ * @param {ToggleLikeTweetProps} props - The properties needed to toggle the like status of a tweet.
+ * @param {boolean} props.isPending - A flag indicating whether the like action is pending.
+ * @param {function} props.startTransition - A function to start a transition.
+ * @param {boolean} props.liked - A flag indicating whether the tweet is liked.
+ * @param {string} props.userId - The ID of the user.
+ * @param {string} props.currentUserId - The ID of the current user.
+ * @param {string} props.threadId - The ID of the tweet thread.
+ * @param {string} props.path - The path of the tweet.
+ * @return {void} No return value.
+ */
 export const toggleLikeTweet = ({
 	isPending,
 	startTransition,
@@ -73,7 +117,7 @@ export const toggleLikeTweet = ({
 	currentUserId,
 	threadId,
 	path,
-}: ToggleLikeTweetProps) => {
+}: ToggleLikeTweetProps): void => {
 	if (isPending) return;
 
 	startTransition(() => {
@@ -94,13 +138,17 @@ export const toggleLikeTweet = ({
 	});
 };
 
-// generate by chatGPT
-export const renderText = (text: string) => {
+/**
+ * Removes empty lines and multiple line breaks from the given text.
+ *
+ * @param {string} text - The text to be processed.
+ * @return {string} The processed text without empty lines and multiple line breaks.
+ */
+export const renderText = (text: string): string => {
 	const textWithoutEmptyLines = text.replace(/^\s*$/gm, " ");
 	const textWithSingleLineBreaks = textWithoutEmptyLines.replace(/\n+/g, "\n");
 	return textWithSingleLineBreaks;
 };
 
-// https://github.com/timdereaper1/caching-forms-in-react
 export const URL_REGEX =
 	/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
